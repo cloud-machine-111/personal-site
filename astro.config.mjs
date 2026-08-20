87e51494d314439468c2d50f5551335540898b5f
Ran <ran_zhao@brown.edu>
2026-08-20 14:01:51 -0400

    yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+     site: 'https://cloud-machine-111.github.io',
+     base: '/personal-site',
 
     fonts: [{
         provider: fontProviders.local(),
